5fa945237204f855fd18e5ac8eb2723edfa00de8
Paweł Sowa <pawel@the-sowa.com>
2022-07-03 13:16:05 +0200

    wip: console log an object

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,23 @@
+use serde::{Deserialize, Serialize};
 use yew::prelude::*;
+use gloo::console::log;
+
+#[derive(Serialize, Deserialize )]
+struct Test {
+    username: String,
+    lang: String,
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let name = String::from("Paweł");
+    let test = Test{
+        username: name,
+        lang: "Rust".to_owned()
+    };
+
+
+    log!(serde_json::to_string_pretty(&test).unwrap());
     html! {
         <h1>{"Hello!"}</h1>
     }
